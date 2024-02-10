a87f59815b4074018d35957b88ab810c67ffa6f0
Pol <polepie@gmail.com>
2024-02-10 16:30:30 -0500

    Add fantom testnet

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x13fcE0acbe6Fb11641ab753212550574CaD31415'
+export const FACTORY_ADDRESS = '0x334C8154f1f6AB1251D16CAe288beB2744C814C8'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
